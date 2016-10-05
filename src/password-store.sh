5fc2f562429e19a1534e3a06d4c8dac8982d5b01
David Cantrell <david.l.cantrell@gmail.com>
2016-10-05 09:47:34 -0400

    Change the shell to #!/bin/sh
    To increase the portability of this script to systems that do not have
    bash or have a version of bash that may or may not correctly execute
    this script, begin the conversion to /bin/sh as the shell.

--- src/password-store.sh
+++ src/password-store.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # Copyright (C) 2012 - 2014 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
 # This file is licensed under the GPLv2+. Please see COPYING for more information.
